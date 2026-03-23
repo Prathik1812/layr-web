27e9ab0d8325e6a855cabe3dc7d018d95c56d38f
Prathik <6011prathik@gmail.com>
2026-03-23 14:55:25 +0530

    Restructure repository into frontend and backend setup

---
+++ backend/src/main/java/com/prathik/layrapi/LayrApiApplication.java
@@ -0,0 +1,13 @@
+package com.prathik.layrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayrApiApplication.class, args);
+	}
+
+}
